3b2a988e123e621f4c35b02cefdb91ad8c87498a
Jake <Jake_Guy_11@protonmail.ch>
2021-06-22 14:32:30 -0400

    added debug argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
 fn main() {
-    println!("Hello, world!");
+    // First collect all the cli args into a vector
+    let cli_args: Vec<String> = std::env::args().skip(1).collect();
+
+    // Check for debug mode first
+    let debug_flag = String::from("--debug");
+    let DEBUG = if cli_args.contains(&debug_flag) { println! ("Debug mode enabled"); true } else { false };
+
+    // Go through each argument
+    for i in 0..cli_args.len()
+    {
+    }
 }
